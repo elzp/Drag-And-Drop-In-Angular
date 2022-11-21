a4b27da4e75e83bf731811579e9dcfcbea06d756
elzp <86740659+elzp@users.noreply.github.com>
2022-11-21 19:38:10 +0100

    Add filtering articles by containers' name

--- src/app/article-container.component.ts
+++ src/app/article-container.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'article-container',
   templateUrl: './article-container.component.html',
   styles: [`h1 { font-family: Lato; }`],
 })
-export class ArticleContainer {
+export class ArticleContainer implements OnInit {
   @Input() name: string;
   @Input() articlesData;
+  thisArticles; 
+
+  ngOnInit(){
+    this.thisArticles = this.articlesData.filter(it=>it.container===this.name)
+  }
 }
