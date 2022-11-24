f8bd7a805823ccecff73008ef867746d3c2fd652
elzp <86740659+elzp@users.noreply.github.com>
2022-11-24 19:35:32 +0100

    Add gettting of proper values of articles' position using ngAfterViewInit

--- src/app/onearticle.component.ts
+++ src/app/onearticle.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, ElementRef } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'onearticle',
   templateUrl: './onearticle.component.html',
   styles: [`h1 { font-family: Lato; }`],
 })
-export class OneArticle {
+export class OneArticle implements AfterViewInit {
   @Input() name: string;
   @Input() title = '';
   @Input() content = '';
@@ -13,7 +13,11 @@ export class OneArticle {
 
   constructor(private el: ElementRef) {
     //from https://stackoverflow.com/a/69948220
+  }
+
+  ngAfterViewInit() {
+    // from https://angular.io/api/core/AfterViewInit#ngAfterViewInit
     const rect = this.el.nativeElement.getBoundingClientRect();
-    console.log(this.name, rect);
+    console.log(this.title, rect);
   }
 }
